f4bdd60c04b2d9222d56d41aa79adbdad819aa0e
yanghengx <yanghengx@nonobank.com>
2017-12-18 15:12:35 +0800

    redirect时传递数据的两种方式，url和flash属性

--- src/main/java/controller/SpitterController.java
+++ src/main/java/controller/SpitterController.java
@@ -11,6 +11,7 @@ import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestMethod;
 import org.springframework.web.bind.annotation.RequestPart;
 import org.springframework.web.multipart.MultipartFile;
+import org.springframework.web.servlet.mvc.support.RedirectAttributes;
 
 import javax.validation.Valid;
 import java.io.File;
@@ -41,15 +42,31 @@ public class SpitterController {
     }
 
     @RequestMapping(value = "/register", method = RequestMethod.POST)
-    public String processRegistration(@Valid Spitter spitter, Errors errors) {
+    public String processRegistration(@Valid Spitter spitter, Model model, Errors errors) {
         //使用注解启用校验
         if (errors.hasErrors()) {
             return "registerForm";
         }
         spitterRepository.save(spitter);
-        //重定向到基本信息页
-        return "redirect:/spitter/" + spitter.getUsername();
+        model.addAttribute("username", spitter.getUsername());
+        model.addAttribute("spitterId", null);
+        //重定向到基本信息页，url拼接
+//        return "redirect:/spitter/" + spitter.getUsername();
+        //重定向,url模板占位符方式
+//        重定向String并没有太大的变化。但是,因为模型中的spitterId属性没有匹配重定 向URL中的任何占位符,所以它会自动以查询参数的形式附加到重定向URL上。
+//        如果username属性的值是habuma并且spitterId属性的值是42,那么结果得到的重定向URL 路径将会是“/spitter/habuma?spitterId=42”
+        return "redirect:/spitter/{username}";
     }
+
+    @RequestMapping(value = "/register", method = RequestMethod.POST)
+    public String processRegistrationWithFlash(@Valid Spitter spitter, RedirectAttributes model) {
+        spitterRepository.save(spitter);
+        model.addAttribute("username", spitter.getUsername());
+        model.addFlashAttribute("spitter", spitter);
+        //重定向到基本信息页，使用Flash属性
+        return "redirect:/spitter/{username}";
+    }
+
 
     @RequestMapping(value = "/{username}", method = RequestMethod.GET)
     public String showSpitterProfile(@PathVariable String username, Model model) {
@@ -73,7 +90,6 @@ public class SpitterController {
         return "registerForm";
 
     }
-
 
 
 }
