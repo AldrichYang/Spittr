c3eb95bb7f3227fc152eee2f12bb1f6e9d04d8b2
yanghengx <yanghengx@nonobank.com>
2017-12-27 15:36:45 +0800

    默认用户配置，失败，先不管了

---
+++ src/main/java/security/MyUserService.java
@@ -0,0 +1,24 @@
+package security;
+
+import org.springframework.security.core.GrantedAuthority;
+import org.springframework.security.core.authority.SimpleGrantedAuthority;
+import org.springframework.security.core.userdetails.User;
+import org.springframework.security.core.userdetails.UserDetails;
+import org.springframework.security.core.userdetails.UserDetailsService;
+import org.springframework.security.core.userdetails.UsernameNotFoundException;
+
+import java.util.ArrayList;
+import java.util.List;
+
+/**
+ * Created by yh on 17/12/27.
+ */
+public class MyUserService implements UserDetailsService {
+
+    @Override
+    public UserDetails loadUserByUsername(String username) throws UsernameNotFoundException {
+        List<GrantedAuthority> authorityList = new ArrayList<>();
+        authorityList.add(new SimpleGrantedAuthority("USER"));
+        return new User("yh", "yh", authorityList);
+    }
+}
--- src/main/java/security/SecurityConfig.java
+++ src/main/java/security/SecurityConfig.java
@@ -27,17 +27,20 @@ public class SecurityConfig extends WebSecurityConfigurerAdapter {
 
     /**
      * 通过重载,配置如何通过拦截器保护请求
+     * 配置如何保护HTTP请求,以及客户端认证用户的方案，支持基于表单的登录以及HTTP Basic方式的认证
      *
      * @param http
      * @throws Exception
      */
     @Override
     protected void configure(HttpSecurity http) throws Exception {
-        super.configure(http);
+//        super.configure(http);
+        http.formLogin().and().authorizeRequests().anyRequest().permitAll();
     }
 
     /**
-     * 通过重载，配置UserDetail服务
+     * 通过重载，配置用户存储
+     * 配置用户存储支撑认证过程
      *
      * @param auth
      * @throws Exception
@@ -45,6 +48,10 @@ public class SecurityConfig extends WebSecurityConfigurerAdapter {
     @Override
     protected void configure(AuthenticationManagerBuilder auth) throws Exception {
         super.configure(auth);
+//        内存用户存储
+//        auth.inMemoryAuthentication().withUser("user").password("password").roles("user").and()
+//                .withUser("admin").password("password").roles("user", "admin");
+        auth.userDetailsService(new MyUserService());
     }
 }
 
